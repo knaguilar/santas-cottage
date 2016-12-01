f3243f55a008a8d85924f12b865d7c728ca7e628
Karla Aguilar <kn.aguilar7@gmail.com>
2016-12-01 15:09:06 -0500

    refactor: AppComponent into a new AppComponent and a SessionsComponent

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,13 +1,17 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule }    from '@angular/forms';
 
 import { AppComponent }  from './app.component';
 import { SessionDetailComponent } from './session-detail.component';
+import { SessionsComponent }     from './sessions.component';
+import { SessionService }         from './session.service';
 
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent, SessionDetailComponent ],
+  imports:      [ BrowserModule, FormsModule ],
+  declarations: [ AppComponent, SessionDetailComponent, SessionsComponent ],
+  providers: [ SessionService ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
